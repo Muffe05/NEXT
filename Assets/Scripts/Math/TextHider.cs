78ed581ef1b77ec36aee45f93105fa30904c08a4
Th1nk <Tobiasransborg@hotmail.com>
2023-04-13 16:08:42 +0200

    MathUniversePart1
    Doesn't look very good, but it's progress nonetheless

--- Assets/Scripts/Math/TextHider.cs
+++ Assets/Scripts/Math/TextHider.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class textHider : MonoBehaviour
+public class TextHider : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public GameObject Text;
+
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "Player")
+        {
+
+
+
+
+            Text.SetActive(false);
+
+
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+    
 }
