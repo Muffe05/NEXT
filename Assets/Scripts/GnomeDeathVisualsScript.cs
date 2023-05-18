8970ba974b2191862af1e688dd1c534977cc4854
Th1nk <Tobiasransborg@hotmail.com>
2023-05-18 19:18:23 +0200

    Math 99,9% Done
    don't revert... pls

--- Assets/Scripts/GnomeDeathVisualsScript.cs
+++ Assets/Scripts/GnomeDeathVisualsScript.cs
@@ -10,6 +10,7 @@ public class VisualEffectsClear : MonoBehaviour
     private Vignette vignette;
     public GameObject PostProcessObject;
     private ColorGrading colorGrading;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,7 @@ public class VisualEffectsClear : MonoBehaviour
         postProcessVolume = PostProcessObject.GetComponent<PostProcessVolume>();
         postProcessVolume.profile.TryGetSettings(out vignette);
         postProcessVolume.profile.TryGetSettings(out colorGrading);
+         
     }
 
     void OnTriggerEnter(Collider other)
@@ -26,7 +28,7 @@ public class VisualEffectsClear : MonoBehaviour
 
 
 
-
+            
             vignette.intensity.value = 0.133f;
             vignette.smoothness.value = 1f;
             colorGrading.saturation.value = 0f;
